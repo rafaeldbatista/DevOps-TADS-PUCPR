ad79e1d406d6fb5bed88aa65b7eaba7b8adac155
Rafael Batista <rafael.diego@pucpr.edu.br>
2026-04-20 22:05:04 -0300

    Update root endpoint response and add user and search endpoints

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"message": "Hello World 🚀"}
 
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return {"user_id": user_id}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/search")
+def search(q: str):
+    return {"query": q}
